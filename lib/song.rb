063f8a6b226bf13aa1e67fdb7b3578e098954c09
Leon <hualiang.tian@gmail.com>
2021-04-03 14:44:10 -0400

    commit tree bdb8b0fc1fb9e38f8eddd5d4a1ad372b3a186488

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,29 @@
+class Song
+    
+    @@all = []
+
+    attr_reader :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+         @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
+
+    # def genre
+    #     Genre.all.select {|genre| genre.song == self}
+    # end
+
+    # def artist
+    #     Artist.all.select {|artist| artist.song == self}
+    # end
+
+
+end
 
